09310d2df2a763e8abce197db02e83cfeadf6451
Bill Krahmer <billism@gmail.com>
2024-05-12 18:51:32 -0500

    Working build with esp32-s3 wroom and camera module.

--- src/custom02/main.cpp
+++ src/custom02/main.cpp
@@ -1,20 +1,53 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 #include <IRremote.hpp>
+#include <WiFi.h>
+#include "esp_camera.h"
+#include "SD_MMC.h"           // SD Card ESP32
+
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char *ssid = "The Multiverse";
+const char *password = "beer is good 123";
+
+const long cameraInterval = 10000;
+unsigned long previousMillis = 0;
+
+// REPLACE WITH YOUR TIMEZONE STRING: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+String myTimezone = "CST6CDT,M3.2.0,M11.1.0";
+
+// ===================
+// Select camera model
+// ===================
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
+//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+// ** Espressif Internal Boards **
+//#define CAMERA_MODEL_ESP32_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S3_CAM_LCD
+
+#include "camera_pins.h"
+
+#define LED_PIN LED_BUILTIN
 
 #define DECODE_NEC // defines the type of IR transmission to decode based on the remote. See IRremote library for examples on how to decode other types of remote
 
-// Purple wire (IR receiver)
-#define IR_PIN 12
-
+// Purple wire (IR receiver) (Pin 2 on freenove_esp32_s3_wroom is an on-board LED indicator)
+#define IR_PIN 14
+#define IR_FEEDBACK_PIN 2
 // Blue wire (Rotation)
-#define YAW_PIN 13
-
+#define YAW_PIN 21
 // Green wire (Pitch - up and down)
-#define PITCH_PIN 15
-
+#define PITCH_PIN 47
 // Yellow wire (Rotation of top half of turret)
-#define ROLL_PIN 14
+#define ROLL_PIN 1
 
 /*
 ** if you want to add other remotes (as long as they're on the same protocol above):
@@ -24,6 +57,11 @@
 ** this will let you add new functions to buttons on other remotes!
 ** the best remotes to try are cheap LED remotes, some TV remotes, and some garage door openers
 */
+
+// SD Card pins
+#define SD_MMC_CMD 38 //Please do not modify it.
+#define SD_MMC_CLK 39 //Please do not modify it. 
+#define SD_MMC_D0  40 //Please do not modify it.
 
 // defines the specific command code for each button on the remote
 #define left 0x8
@@ -68,6 +106,9 @@ int rollPrecision = 158; // this variable represents the time in milliseconds th
 int pitchMax = 175; // this sets the maximum angle of the pitch servo to prevent it from crashing, it should remain below 180, and be greater than the pitchMin
 int pitchMin = 10;  // this sets the minimum angle of the pitch servo to prevent it from crashing, it should remain above 0, and be less than the pitchMax
 
+// Stores the camera configuration parameters
+camera_config_t config;
+
 void yosemiteSam();
 void shakeHeadYes(int moves);
 void shakeHeadNo(int moves);
@@ -79,32 +120,270 @@ void fire();
 void fireAll();
 void homeServos();
 
+// Initializes the camera
+void configInitCamera()
+{
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+   // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  // for larger pre-allocated frame buffer.
+  if(psramFound()){
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+    config.grab_mode = CAMERA_GRAB_LATEST;
+  } else {
+    // Limit the frame size when PSRAM is not available
+    config.frame_size = FRAMESIZE_SVGA;
+    config.fb_location = CAMERA_FB_IN_DRAM;
+  }
+
+  // Serial.println("Setting camera for lower quality. (FRAMESIZE_VGA)");
+  // config.frame_size = FRAMESIZE_VGA;
+  // config.jpeg_quality = 18;
+  // config.fb_count = 1;
+
+  // Initialize the Camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  s->set_vflip(s, 1); // flip it back
+  s->set_brightness(s, 1); // up the brightness just a bit
+  s->set_saturation(s, 0); // lower the saturation
+}
+
+// Connect to wifi
+void initWiFi()
+{
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting Wifi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+}
+
+// Function to set timezone
+void setTimezone(String timezone)
+{
+  Serial.printf("  Setting Timezone to %s\n", timezone.c_str());
+  setenv("TZ", timezone.c_str(), 1); //  Now adjust the TZ.  Clock settings are adjusted to show the new local time
+  tzset();
+}
+
+// Connect to NTP server and adjust timezone
+void initTime(String timezone)
+{
+  struct tm timeinfo;
+  Serial.println("Setting up time");
+  configTime(0, 0, "pool.ntp.org"); // First connect to NTP server, with 0 TZ offset
+  if (!getLocalTime(&timeinfo))
+  {
+    Serial.println(" Failed to obtain time");
+    return;
+  }
+  Serial.println("Got the time from NTP");
+  // Now we can set the real timezone
+  setTimezone(timezone);
+}
+
+// Get the picture filename based on the current ime
+String getPictureFilename()
+{
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    Serial.println("Failed to obtain time");
+    return "";
+  }
+  char timeString[20];
+  strftime(timeString, sizeof(timeString), "%Y-%m-%d_%H-%M-%S", &timeinfo);
+  Serial.println(timeString);
+  String filename = "/picture_" + String(timeString) + ".jpg";
+  return filename;
+}
+
+// Initialize the micro SD card
+void initMicroSDCard()
+{
+  // Start Micro SD card
+  Serial.println("Starting SD Card");
+
+  SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
+  if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5))
+  {
+    Serial.println("SD Card Mount Failed");
+    return;
+  }
+
+  uint8_t cardType = SD_MMC.cardType();
+  if (cardType == CARD_NONE)
+  {
+    Serial.println("No SD Card attached");
+    return;
+  }
+
+  Serial.print("SD_MMC Card Type: ");
+  if (cardType == CARD_MMC)
+  {
+    Serial.println("MMC");
+  }
+  else if (cardType == CARD_SD)
+  {
+    Serial.println("SDSC");
+  }
+  else if (cardType == CARD_SDHC)
+  {
+    Serial.println("SDHC");
+  }
+  else
+  {
+    Serial.println("UNKNOWN");
+  }
+}
+
+// Take photo and save to microSD card
+void takePhotoAndSave()
+{
+  Serial.println("\ntakePhotoAndSave(): Begin");
+
+  // Take Picture with Camera
+  camera_fb_t *fb = esp_camera_fb_get();
+
+  // Uncomment the following lines if you're getting old pictures
+  // esp_camera_fb_return(fb); // dispose the buffered image
+  // fb = NULL; // reset to capture errors
+  // fb = esp_camera_fb_get();
+
+  if (!fb)
+  {
+    Serial.println("Camera capture failed. Restarting...");
+    delay(1000);
+    ESP.restart();
+  }
+
+  // Path where new picture will be saved in SD Card
+  Serial.println("\ntakePhotoAndSave(): getPictureFilename");
+  String path = getPictureFilename();
+  Serial.printf("Picture file name: %s\n", path.c_str());
+
+  // Save picture to microSD card
+  Serial.println("\ntakePhotoAndSave(): Open file for output...");
+  fs::FS &fs = SD_MMC;
+  File file = fs.open(path.c_str(), FILE_WRITE);
+  if (!file)
+  {
+    Serial.printf("Failed to open file in writing mode");
+  }
+  else
+  {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.printf("Saved: %s\n", path.c_str());
+  }
+
+  Serial.println("\ntakePhotoAndSave(): Close file...");
+  file.close();
+  esp_camera_fb_return(fb);
+
+  Serial.println("\ntakePhotoAndSave(): End");
+}
+
 //////////////////////////////////////////////////
 //  S E T U P  //
 //////////////////////////////////////////////////
 void setup()
 {
+  // https://www.instructables.com/ESP32-CAM-Take-Photo-and-Save-to-MicroSD-Card-With/
+
   Serial.begin(115200); // initializes the Serial communication between the computer and the microcontroller
 
-  // delay(7000);
+  delay(2000);
 
   Serial.println("Starting...");
+
+  // initialize digital pin LED_PIN as an output
+  pinMode(LED_PIN, OUTPUT);
+
+  // // Blink thrice to indicate status
+  // digitalWrite(LED_PIN, HIGH);
+  // delay(100);
+  // digitalWrite(LED_PIN, LOW);
+  // delay(100);
+  // digitalWrite(LED_PIN, HIGH);
+  // delay(100);
+  // digitalWrite(LED_PIN, LOW);
+  // delay(100);
+  // digitalWrite(LED_PIN, HIGH);
+  // delay(100);
+  // digitalWrite(LED_PIN, LOW);
+
+  // Initialize Wi-Fi
+  Serial.println("\nInitializing wifi...");
+  initWiFi();
+  Serial.println("\nDone initializing wifi - OK!");
+
+  // Initialize time with timezone
+  Serial.println("\nInitializing time...");
+  initTime(myTimezone);
+  Serial.println("\nDone initializing time - OK!");
+
+  // Initialize the camera
+  Serial.println("\nInitializing the camera module...");
+  configInitCamera();
+  Serial.println("\nDone initializing the camera module - OK!");
+
+  // Initialize MicroSD
+  Serial.println("\nInitializing the micro sd card module...");
+  initMicroSDCard();
+  Serial.println("\nDone initializing the micro sd card module - OK!");
 
   yawServo.attach(YAW_PIN);     // attach YAW servo to pin YAW_PIN
   pitchServo.attach(PITCH_PIN); // attach PITCH servo to pin 11
   rollServo.attach(ROLL_PIN);   // attach ROLL servo to pin 12
 
-  // Just to know which program is running on my microcontroller
-  Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+  Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing IR Remote library version " VERSION_IRREMOTE));
 
   // Start the receiver and if not 3. parameter specified, take LED_BUILTIN pin from the internal boards definition as default feedback LED
-  IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
+  Serial.print("\nInitializing the IR receiver...");
+  IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK, IR_FEEDBACK_PIN);
+  Serial.println("\nDone initializing the IR receiver - OK!");
 
-  Serial.print(F("Ready to receive IR signals of protocols: "));
+  Serial.print(F("\nReady to receive IR signals of protocols: "));
   printActiveIRProtocols(&Serial);
-  Serial.println("at pin 9");
 
+  Serial.println("\nHoming servos...");
   homeServos(); // set servo motors to home position
+  Serial.println("Done homing servos.");
 }
 
 ////////////////////////////////////////////////
@@ -113,80 +392,99 @@ void setup()
 
 void loop()
 {
-  /*
-   * Check if received data is available and if yes, try to decode it.
-   */
-  if (IrReceiver.decode())
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= cameraInterval)
   {
+    previousMillis = currentMillis;
 
-    /*
-     * Print a short summary of received data
-     */
-    IrReceiver.printIRResultShort(&Serial);
-    IrReceiver.printIRSendUsage(&Serial);
-    if (IrReceiver.decodedIRData.protocol == UNKNOWN)
-    { // command garbled or not recognized
-      Serial.println(F("Received noise or an unknown (or not yet enabled) protocol - if you wish to add this command, define it at the top of the file with the hex code printed below (ex: 0x8)"));
-      // We have an unknown protocol here, print more info
-      IrReceiver.printIRResultRawFormatted(&Serial, true);
-    }
-    Serial.println();
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
 
-    /*
-     * !!!Important!!! Enable receiving of the next value,
-     * since receiving has stopped after the end of the current received data packet.
-     */
-    IrReceiver.resume(); // Enable receiving of the next value
+    takePhotoAndSave();
 
-    /*
-     * Finally, check the received data and perform actions according to the received command
-     */
-
-    switch (IrReceiver.decodedIRData.command)
-    { // this is where the commands are handled
-
-    case up: // pitch up
-      upMove(1);
-      break;
-
-    case down: // pitch down
-      downMove(1);
-      break;
-
-    case left: // fast counterclockwise rotation
-      leftMove(1);
-      break;
-
-    case right: // fast clockwise rotation
-      rightMove(1);
-      break;
-
-    case ok: // firing routine
-      fire();
-      // Serial.println("FIRE");
-      break;
-
-    case star:
-      fireAll();
-      delay(50);
-      break;
-
-    case cmd0:
-      shakeHeadNo(3);
-      delay(50);
-      break;
-
-    case cmd9:
-      shakeHeadYes(3);
-      delay(50);
-      break;
-
-    case cmd7:
-      yosemiteSam();
-      break;
-    }
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
   }
-  delay(5);
+
+  // /*
+  //  * Check if received data is available and if yes, try to decode it.
+  //  */
+  // if (IrReceiver.decode())
+  // {
+  //   /*
+  //    * Print a short summary of received data
+  //    */
+  //   IrReceiver.printIRResultShort(&Serial);
+  //   IrReceiver.printIRSendUsage(&Serial);
+  //   if (IrReceiver.decodedIRData.protocol == UNKNOWN)
+  //   { // command garbled or not recognized
+  //     Serial.println(F("Received noise or an unknown (or not yet enabled) protocol - if you wish to add this command, define it at the top of the file with the hex code printed below (ex: 0x8)"));
+  //     // We have an unknown protocol here, print more info
+  //     IrReceiver.printIRResultRawFormatted(&Serial, true);
+  //   }
+  //   Serial.println();
+
+  //   /*
+  //    * !!!Important!!! Enable receiving of the next value,
+  //    * since receiving has stopped after the end of the current received data packet.
+  //    */
+  //   IrReceiver.resume(); // Enable receiving of the next value
+
+  //   /*
+  //    * Finally, check the received data and perform actions according to the received command
+  //    */
+
+  //   switch (IrReceiver.decodedIRData.command)
+  //   { // this is where the commands are handled
+
+  //   case up: // pitch up
+  //     upMove(1);
+  //     break;
+
+  //   case down: // pitch down
+  //     downMove(1);
+  //     break;
+
+  //   case left: // fast counterclockwise rotation
+  //     leftMove(1);
+  //     break;
+
+  //   case right: // fast clockwise rotation
+  //     rightMove(1);
+  //     break;
+
+  //   case ok: // firing routine
+  //     fire();
+  //     // Serial.println("FIRE");
+  //     break;
+
+  //   case star:
+  //     fireAll();
+  //     delay(50);
+  //     break;
+
+  //   case cmd0:
+  //     shakeHeadNo(3);
+  //     delay(50);
+  //     break;
+
+  //   case cmd9:
+  //     shakeHeadYes(3);
+  //     delay(50);
+  //     break;
+
+  //   case cmd7:
+  //     yosemiteSam();
+  //     break;
+  //   }
+  // }
+  // delay(5);
 }
 
 /// @brief Shoot around aimlessly.
